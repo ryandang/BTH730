52e4b5f5e92a40942379e4fd3c4974c4f3eced72
Ryan Dang <ryandang.cpa@gmail.com>
2014-11-22 00:55:58 -0500

    Add most functionalities

--- js/loginController.js
+++ js/loginController.js
@@ -20,109 +20,89 @@ angular.module('bthApp', ["ui.bootstrap", "ngSanitize"])
 
 })
 .controller("viewController", function($scope, $modal) {
-  $scope.allPosts = [
-    {
-      id: 1,
-      owner: "Ryan Dang",
-      profile: "images/profile.png",
-      title: "Book title 1",
-      type: "<a href='#'>#Sell</a>",
-      image: null,
-      price: 25,
-      reply: 2,
-      fav: 1,
-      isFaved: false,
-      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
-      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
-    },
-    {
-      id: 2,
-      owner: "Luong Chuong",
-      profile: "images/profile2.png",
-      title: "Ken Follett",
-      type: "<a href='#'>#Sell</a>",
-      image: "images/book1.jpg",
-      price: 20,
-      reply: 12,
-      fav: 3,
-      isFaved: true,
-      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
-      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
-    },
-    {
-      id: 3,
-      owner: "Ryan Dang",
-      profile: "images/profile.png",
-      title: "Sylvia Day",
-      type: "<a href='#'>#Sell</a>",
-      image: "images/book2.jpg",
-      price: 25,
-      reply: 5,
-      fav: 2,
-      isFaved: false,
-      content: "Gideon calls me his angel, but he’s the miracle in my life. My gorgeous, wounded warrior, so determined to slay my demons while refusing to face his own.The vows we'd exchanged should have bound us tighter than blood and flesh. Instead they opened old wounds, exposed pain and insecurities, and lured bitter enemies out of the shadows. I felt him slipping from my grasp, my greatest fears becoming my reality, my love tested in ways I wasn’t sure I was strong enough to bear.At the brightest time in our lives, the darkness of his past encroached and threatened everything we’d worked so hard for. We faced a terrible choice: the familiar safety of the lives we’d had before each other or the fight for a future that suddenly seemed an impossible and hopeless dream…",
-      campuses: ["<a href='#'>#King</a>"]
-    }
-  ];
-$scope.content = "<a href='#'>#Sell</a> Book title 1";
-  $scope.myFavs = [
-    {
-      id: 2,
-      owner: "Luong Chuong",
-      profile: "images/profile2.png",
-      title: "Ken Follett",
-      type: "<a href='#'>#Sell</a>",
-      image: "images/book1.jpg",
-      price: 20,
-      reply: 12,
-      fav: 3,
-      isFaved: true,
-      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
-      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
-    }
-  ];
-  $scope.myPosts = [
-    {
-      id: 1,
-      owner: "Ryan Dang",
-      profile: "images/profile.png",
-      title: "Book title 1",
-      type: "<a href='#'>#Sell</a>",
-      image: null,
-      price: 25,
-      reply: 2,
-      fav: 1,
-      isFaved: false,
-      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
-      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
-    },
-    {
-      id: 3,
-      owner: "Ryan Dang",
-      profile: "images/profile.png",
-      title: "Sylvia Day",
-      type: "<a href='#'>#Sell</a>",
-      image: "images/book2.jpg",
-      price: 25,
-      reply: 5,
-      fav: 2,
-      isFaved: false,
-      content: "Gideon calls me his angel, but he’s the miracle in my life. My gorgeous, wounded warrior, so determined to slay my demons while refusing to face his own.The vows we'd exchanged should have bound us tighter than blood and flesh. Instead they opened old wounds, exposed pain and insecurities, and lured bitter enemies out of the shadows. I felt him slipping from my grasp, my greatest fears becoming my reality, my love tested in ways I wasn’t sure I was strong enough to bear.At the brightest time in our lives, the darkness of his past encroached and threatened everything we’d worked so hard for. We faced a terrible choice: the familiar safety of the lives we’d had before each other or the fight for a future that suddenly seemed an impossible and hopeless dream…",
-      campuses: ["<a href='#'>#King</a>"]
-    }
-  ];
 
-  $scope.addFav = function(id, e) {
-    console.log(id);
-    var tempPost;
-    // console.log(e);
+  $scope.allPosts = [];
+  $scope.myPosts = [];
+  $scope.myFavs = [];
+
+  var item1 = {
+      id: 1,
+      owner: "Ryan Dang",
+      profile: "images/profile.png",
+      title: "Book title 1",
+      type: "<a href='#'>#Sell</a>",
+      image: null,
+      price: 25,
+      reply: 2,
+      fav: 1,
+      isFaved: false,
+      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
+      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
+    };
+
+    var item2 =
+    {
+      id: 2,
+      owner: "Luong Chuong",
+      profile: "images/profile4.png",
+      title: "Ken Follett",
+      type: "<a href='#'>#Sell</a>",
+      image: "images/book1.jpg",
+      price: 20,
+      reply: 12,
+      fav: 3,
+      isFaved: true,
+      content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
+      campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
+    };
+
+    var item3 = {
+      id: 3,
+      owner: "Ryan Dang",
+      profile: "images/profile.png",
+      title: "Sylvia Day",
+      type: "<a href='#'>#Sell</a>",
+      image: "images/book2.jpg",
+      price: 25,
+      reply: 5,
+      fav: 2,
+      isFaved: false,
+      content: "Gideon calls me his angel, but he’s the miracle in my life. My gorgeous, wounded warrior, so determined to slay my demons while refusing to face his own.The vows we'd exchanged should have bound us tighter than blood and flesh. Instead they opened old wounds, exposed pain and insecurities, and lured bitter enemies out of the shadows. I felt him slipping from my grasp, my greatest fears becoming my reality, my love tested in ways I wasn’t sure I was strong enough to bear.At the brightest time in our lives, the darkness of his past encroached and threatened everything we’d worked so hard for. We faced a terrible choice: the familiar safety of the lives we’d had before each other or the fight for a future that suddenly seemed an impossible and hopeless dream…",
+      campuses: ["<a href='#'>#King</a>"]
+    };
+
+    var item4 = {
+      id: 5,
+      owner: "Steven Chun",
+      profile: "images/profile3.png",
+      title: "Information Technology system",
+      type: "<a href='#'>#Sell</a>",
+      image: "images/book3.jpg",
+      price: 25,
+      reply: 25,
+      fav: 6,
+      isFaved: false,
+      content: "The book original cost is $135. I'm selling it for really <a href='#'>#cheap</a> This book is for <a href='#'>#BTS730</a> <a href='#'>#BSD</a> program",
+      campuses: ["<a href='#'>#King</a>"]
+    };
+
+    $scope.allPosts.push(item1);
+    $scope.allPosts.push(item2);
+    $scope.allPosts.push(item3);
+    $scope.allPosts.push(item4);
+    $scope.myPosts.push(item1);
+    $scope.myPosts.push(item3);
+    $scope.myFavs.push(item2);
+
+  $scope.postId = 45;
+
+  $scope.addFav = function(id) {
     _.each($scope.allPosts, function(post) {
       if(post.id === id) {
-        tempPost = post;
         post.isFaved = !post.isFaved;
         if(post.isFaved) {
           post.fav++;
-          $scope.myFavs.push(tempPost);
+          $scope.myFavs.push(post);
         } else {
           post.fav--;
           $scope.myFavs = _.reject($scope.myFavs, function(post) {
@@ -131,20 +111,10 @@ $scope.content = "<a href='#'>#Sell</a> Book title 1";
         }
       }
     });
-
-    _.each($scope.myPosts, function(post) {
-      if(post.id === id) {
-        post.isFaved = !post.isFaved;
-        if(post.isFaved) {
-          post.fav++;
-        } else {
-          post.fav--;
-        }
-      }
-    });
   };
 
   $scope.addPost = function() {
+    $scope.postId ++;
     var modalInstance = $modal.open({
       templateUrl: 'myModalContent.html',
       controller: 'ModalInstanceCtrl',
@@ -154,18 +124,22 @@ $scope.content = "<a href='#'>#Sell</a> Book title 1";
           },
           myPosts: function() {
               return $scope.myPosts;
+          },
+          postId: function() {
+              return $scope.postId;
           }
       }
     });
   }
 })
 
-.controller('ModalInstanceCtrl', function ($scope, $modalInstance, allPosts, myPosts) {
-
+.controller('ModalInstanceCtrl', function ($scope, $modalInstance, allPosts, myPosts, postId) {
+  $scope.postId = postId;
   $scope.allPosts = allPosts;
   $scope.myPosts = myPosts;
   $scope.isSelling = true;
   $scope.type = "<a href='#'>#Sell</a>";
+  $scope.title = "";
   $scope.titles = [
     "The Hunger Games",
     " Harry Potter and the Order of the Phoenix (Harry Potter, #5) ",
@@ -188,9 +162,10 @@ $scope.content = "<a href='#'>#Sell</a> Book title 1";
 
 
   $scope.ok = function () {
-    $modalInstance.close();
+
     var temp = {};
     var campuses = [];
+    $scope.description = $scope.description || "";
 
     if($scope.selectedYork)
      campuses.push("<a href='#'>#York</a>");
@@ -205,23 +180,27 @@ $scope.content = "<a href='#'>#Sell</a> Book title 1";
 
     if(!$scope.price)
       $scope.price = 0;
-    temp.title = $scope.title;
-    temp.profile = "images/profile.png";
-    temp.price = $scope.price;
-    temp.type = $scope.type;
+    temp.id = $scope.postId;
     temp.owner = "Ryan Dang";
+    temp.profile = "images/profile.png";
+    temp.title = $scope.title;
+    temp.type = $scope.type;
+    temp.image = "images/" + $("#file").val();
+    temp.price = $scope.price;
+    temp.reply = 0;
+    temp.fav = 0;
+    temp.isFaved = false;
     temp.content = $scope.description;
     temp.campuses = campuses;
 
-    $scope.allPosts.push(temp);
-    $scope.myPosts.push(temp);
     var matches = $scope.description.match(/#\w+/g);
-
     _.each(matches, function(hashtag) {
-      console.log(hashtag);
       temp.content = temp.content.replace(hashtag, "<a href='#'>"+hashtag+"</a>");
     });
 
+    $scope.allPosts.push(temp);
+    $scope.myPosts.push(temp);
+    $modalInstance.close();
   };
 
   $scope.cancel = function () {
@@ -234,3 +213,62 @@ $scope.content = "<a href='#'>#Sell</a> Book title 1";
     return items.slice().reverse();
   };
 });
+
+// .factory('Data', function ($rootScope) {
+//   var Data = {};
+//   var postId = 40;
+//   var allPosts = [
+//     {
+//       id: 1,
+//       owner: "Ryan Dang",
+//       profile: "images/profile.png",
+//       title: "Book title 1",
+//       type: "<a href='#'>#Sell</a>",
+//       image: null,
+//       price: 25,
+//       reply: 2,
+//       fav: 1,
+//       isFaved: false,
+//       content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
+//       campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
+//     },
+//     {
+//       id: 2,
+//       owner: "Luong Chuong",
+//       profile: "images/profile2.png",
+//       title: "Ken Follett",
+//       type: "<a href='#'>#Sell</a>",
+//       image: "images/book1.jpg",
+//       price: 20,
+//       reply: 12,
+//       fav: 3,
+//       isFaved: true,
+//       content: "The book is in #perfect condition. The price can be <a href='#'>#negotiated</a>. This book is for <a href='#'>#BTN710</a> <a href='#'>#BSD</a> program",
+//       campuses: ["<a href='#'>#York</a>", "<a href='#'>#King</a>"]
+//     },
+//     {
+//       id: 3,
+//       owner: "Ryan Dang",
+//       profile: "images/profile.png",
+//       title: "Sylvia Day",
+//       type: "<a href='#'>#Sell</a>",
+//       image: "images/book2.jpg",
+//       price: 25,
+//       reply: 5,
+//       fav: 2,
+//       isFaved: false,
+//       content: "Gideon calls me his angel, but he’s the miracle in my life. My gorgeous, wounded warrior, so determined to slay my demons while refusing to face his own.The vows we'd exchanged should have bound us tighter than blood and flesh. Instead they opened old wounds, exposed pain and insecurities, and lured bitter enemies out of the shadows. I felt him slipping from my grasp, my greatest fears becoming my reality, my love tested in ways I wasn’t sure I was strong enough to bear.At the brightest time in our lives, the darkness of his past encroached and threatened everything we’d worked so hard for. We faced a terrible choice: the familiar safety of the lives we’d had before each other or the fight for a future that suddenly seemed an impossible and hopeless dream…",
+//       campuses: ["<a href='#'>#King</a>"]
+//     }
+//   ];
+
+//   Data.getAllPosts = function() {
+//     return allPosts;
+//   }
+
+//   Data.addPost = function(post) {
+//     allPosts.push(post);
+//   }
+
+//   return Data;
+// });
